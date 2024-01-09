494365aac7bdb1486898542117dcf03138605ebb
neck950728 <p_gon_ha_da@naver.com>
2024-01-09 13:58:43 +0900

    Init

---
+++ resources/js/main/desktop.js
@@ -0,0 +1,126 @@
+$(function(){
+	$.ajaxSetup({
+		async:false
+	});
+	
+	$.ajax({
+		url:"/Portfolio/resources/json/main/desktop.json",
+		
+		success:function(json){
+			$.each(json.list, function(index, file){
+				$("#desktop > ul").append("<li id='" + file.name.toLowerCase() + "' class='file'>" +
+														"<img src='" + file.icon + "'><br>" +
+														"<span>" + file.name + "</span>" +
+													"</li>");
+			});
+		}
+	});
+	
+	$("#desktop").fadeIn();
+});
+
+$(function(){
+	$("body").on(getClickType(), "#desktop .file", function(e, isForCopying){
+		var type = $(this).attr("id");
+		
+		var uuid = create_window(type, isForCopying);
+		$(this).data("uuid", uuid);
+		$("#" + uuid + " > #body > .content").attr("id", type);
+		
+		if(type == "about"){
+			$("#" + uuid + " > #body > .content").append("<h1>김민진에 대한 기본 정보 보기</h1>" +
+																		  "<div>" +
+																		  	  "<p>안녕하세요.</p>" +
+																		  	  "<p>신입 백엔드 개발자 김민진입니다.</p>" +
+																		  	  "<p>하나를 배우더라도 제대로 깊이 있게 파헤치고자 하는 것이 제 강점입니다.</p>" +
+																		  	  "<p>기초를 탄탄히 하여 변화에 빠르게 적응할 수 있는 개발자가 되기 위해 노력하겠습니다.</p>" +
+																		  "</div>" +
+																		  "<ul>" +
+																		  	  "<li class='profile'>" +
+																		  	  	  "<p class='boundary'><span>Profile</span></p>" +
+																		  	  	  "<div>" +
+																		  	  	  	  "<h1>김민진</h1>" +
+																		  	  	  	  "<p>Back-End 개발자(신입)</p>" +
+																		  	  	  	  "<p>1995.07.28</p>" +
+																		  	  	  	  "<p>인천광역시 부평구</p>" +
+																		  	  	  	  "<p class='blog'><a href='https://blog.naver.com/dngu_icdi' target='_blank'>blog.naver.com/dngu_icdi</a>(이웃 공개)</p>" +
+																			  	  	  "<div class='blogDescription'>" +
+																		  	  	  	  	  "<p>학습한 내용을 개인 기록 목적으로 작성 중인 비공개 블로그입니다.</p>" +
+																		  	  	  	  	  "<p>과거, 개인 소장이라는 명목하에 출처를 남기지 않고 스크랩하였던 게시물들이 포함되어 있어,</p>" +
+																		  	  	  	  	  "<p>제가 이웃으로 추가한 분들만 게시글을 열람할 수 있도록 해두었습니다.</p>" +
+																		  	  	  	  	  "<p>혹시 관심이 있으신 분들은 '이웃 신청'을 해주신다면,</p>" +
+																		  	  	  	  	  "<p>빠른 시일 내에 확인하여 추가해드리도록 하겠습니다.</p>" +
+																		  	  	  	  	  "<p>죄송합니다.</p>" +
+																		  	  	  	  "</div>" +
+																		  	  	  "</div>" +
+																		  	  	  "<img src='/Portfolio/resources/images/main/about/me.jpg'>" +
+																		  	  "</li>" +
+																		  	  /*
+																			  	  "<li class='experience'>" +
+																			  	  	  "<p class='boundary'><span>Programming Experience</span></p>" +
+																			  	  	  "<ul></ul>" +
+																			  	  "</li>" +
+																				  "<li class='certificate'>" +
+																			  	  	  "<p class='boundary'><span>Certificate</span></p>" +
+																			  	  	  "<ul></ul>" +
+																			  	  "</li>" +
+																		  	  */
+																		  	  "<li class='skills'>" +
+																		  	  	  "<p class='boundary'><span>Skills</span></p>" +
+																		  	  	  "<p class='details'><span onclick=\"window.open('/Portfolio/resources/pdf/skills_details.pdf')\">자세히 보기</span></p>" +
+																		  	  	  "<p class='inProgress'>현재는 React와 JPA를 꾸준히 학습하고 있습니다.</p>" +
+																		  	  	  "<ul></ul>" +
+																		  	  "</li>" +
+																		  "</ul>");
+			
+			$.ajax({
+				url:"/Portfolio/resources/json/main/about.json",
+				async:false,
+				success:function(json){
+					/*
+						$.each(json.experience, function(index, experience){
+							$("#" + uuid + " > #body > .content .experience > ul").append("<li>" +
+																													"<h2>- " + experience.period + "</h2>" +
+																													"<i> : </i>" +
+																													"<span>" + experience.process + "</span>" +
+																												"</li>");
+						});
+						
+						$.each(json.certificate, function(index, certificate){
+							$("#" + uuid + " > #body > .content .certificate > ul").append("<li>" +
+																													"<h2>- " + certificate.name + "(" + certificate.acquisition_date + ")" + "</h2>" +
+																													"<i> : </i>" +
+																													"<span>" + certificate.institution + "</span>" +
+																												"</li>");
+						});
+					*/
+					
+					$.each(json.skills, function(key, value){
+						$.each(value, function(i, val){
+							var class_ = key;
+							if(i == 0){
+								if(key == "server_side"){
+									$("#" + uuid + " > #body > .content .skills > ul").append("<li class='contour'></li>");
+								}
+								
+								class_ += " first";
+							}else if(i == value.length - 1){
+								class_ += " last";
+							}
+							
+							$("#" + uuid + " > #body > .content .skills > ul").append("<li class='" + class_ + "'><img src='" + val.icon + "'></li>");
+						});
+					});
+				}
+			});
+		}else if(type == "project"){
+			compose_directory(uuid, "Project", "/Portfolio/resources/json/main/project.json", false);
+		}else if(type == "contact"){
+			compose_directory(uuid, "Contact", "/Portfolio/resources/json/main/contact.json", false);
+		}
+		
+		var window_height = parseInt($("#" + uuid).css("height"));
+		var header_height = 77;
+		$("#" + uuid + " > #body").css("height", window_height - header_height + "px");
+	});
+});
---
+++ resources/js/main/public.js
@@ -0,0 +1,162 @@
+var curFocusTarget;
+$(function(){
+	$("body").on("click", ".file", function(){
+		if(curFocusTarget != undefined && curFocusTarget.get(0) != $(this).get(0)){
+			curFocusTarget.removeClass("selected");
+		}
+		
+		if(curFocusTarget == undefined || curFocusTarget.get(0) != $(this).get(0)){
+			curFocusTarget = $(this);
+			$(this).addClass("selected");
+		}
+	});
+	
+	$("html").click(function(e){
+		if(curFocusTarget != undefined){
+			if($(e.target).get(0) != curFocusTarget.get(0)){
+				if($(e.target).closest(".file").get(0) != curFocusTarget.get(0)){
+					curFocusTarget.removeClass("selected");
+					curFocusTarget = undefined;
+				}
+			}
+		}
+	});
+});
+
+// ===============================================================================================================
+
+$(function(){
+	$("body").on("keyup", ".chrome input[type='text']", function(e){
+		if(e.keyCode == 13){
+			window.open("https://www.google.com/search?q=" + $(this).val());
+		}
+	});
+});
+
+// ===============================================================================================================
+
+// function compose_directory(uuid, target, data, isDirectoryInFolder = true){ // Internet Explorer : Default Parameters(isDirectoryInFolder = true) 지원 X
+function compose_directory(uuid, target, data, isDirectoryInFolder){
+	isDirectoryInFolder = isDirectoryInFolder == undefined ? true : isDirectoryInFolder;
+	
+	$("#" + uuid + " > #header > .navigationBar > .buttons > ul > li.active").removeClass("active");
+	
+	if(isDirectoryInFolder){
+		$("#" + uuid + " > #header > .navigationBar > .bar > ul").append("<i><img src='/Portfolio/resources/images/main/next.png'></i>" +
+																							   "<li><span>" + target + "</span></li>");
+	}
+	
+	$("#" + uuid + " > #body > .content").addClass("directory");
+	$("#" + uuid + " > #body > .content.directory").empty();
+	$("#" + uuid + " > #body > .content.directory").append("<ul class='header'>" +
+																				   "<li class='name'>이름</li>" +
+																				   "<li class='date'>날짜</li>" +
+																				   "<li class='type'>유형</li>" +
+																			   "</ul>" +
+																			   "<ul class='body'></ul>");
+	
+	$.ajax({
+		url:data,
+		
+		success:function(json){
+			var date = dateFormat(new Date());
+			
+			$.each(json.list, function(index, file){
+				var li_header = "<li id='" + file.id + "' class='file'>";
+				if(file.type == "폴더"){ li_header = "<li id='" + file.id + "' class='file folder'>"; }
+				
+				$("#" + uuid + " > #body > .content.directory > .body").append(li_header +
+																										  "<ul>" +
+																										  	  "<li class='name'>" +
+																										  	  	  "<img src='" + file.icon + "'>" +
+																										  	  	  file.name +
+																										  	  "</li>" +
+																										  	  "<li class='date'>" + ((file.date) ? file.date : date) + "</li>" +
+																										  	  "<li class='type'>" + file.type + "</li>" +
+																										  "</ul>" +
+																									  "</li>");
+				
+				if(file.link != undefined){
+					$(".file#" + file.id).attr((isPC()) ? "ondblclick" : "onclick", "window.open('" + file.link + "')");
+				}else if(file.type == "Details"){
+					$(".file#" + file.id).attr((isPC()) ? "ondblclick" : "onclick", "showDetails('" + file.target + "')");
+				}
+			});
+		}
+	});
+}
+
+function dateFormat(date){
+	var month = date.getMonth() + 1;
+	var day = date.getDate();
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	// var second = date.getSeconds();
+	
+	var meridiem = "오전";
+	if(hour > 12){
+		meridiem = "오후";
+		hour -= 12;
+	}
+	
+	month = month >= 10 ? month : '0' + month;
+	day = day >= 10 ? day : '0' + day;
+	hour = hour >= 10 ? hour : '0' + hour;
+	minute = minute >= 10 ? minute : '0' + minute;
+	// second = second >= 10 ? second : '0' + second;
+	
+	return date.getFullYear() + '-' + month + '-' + day + ' ' + meridiem + ' ' + hour + ':' + minute;
+	// return date.getFullYear() + '-' + month + '-' + day + ' ' + meridiem + ' ' + hour + ':' + minute + ':' + second;
+}
+
+function showDetails(target){
+	$.ajax({
+		url:"/Portfolio/details/" + target + ".html",
+		dataType:"html",
+		success:function(html){
+			let details = $(html).hide();
+			$("body").append(details);
+			details.fadeIn(250);
+		}
+	});
+}
+
+function detailsCommonScript(){
+	$(".slider").slick({
+		prevArrow:"<button type='button' class='slick-prev'>Previous</button>",
+		nextArrow:"<button type='button' class='slick-next'>Next</button>",
+		dots:true
+	});
+	
+	$("video").attr("width", "100%");
+	$("video").attr("controls", "controls");
+	$("video").prop("muted", true);
+	
+	if($(window).height() > 768){
+		$(".section").css("height", $(window).height() * 0.65 + "px");
+	}else if($(window).height() > 480){
+		$(".section").css("height", $(window).height() * 0.75 + "px");
+	}else{
+		$(".section").css("height", $(window).height() + "px");
+	}
+	
+	$("#detailsViewer #close").click(function(){
+		$("#detailsBackground").remove();
+		$(".destroy").remove();
+	});
+}
+
+// ===============================================================================================================
+
+function getClickType(){
+	return (isPC()) ? "dblclick" : "click";
+}
+
+function isPC(){
+	var filter = "win16|win32|win64|mac|macintel";
+	if(filter.indexOf(navigator.platform.toLowerCase()) > 0){
+		return "true";
+	}else{
+		return "false";
+	}
+}
